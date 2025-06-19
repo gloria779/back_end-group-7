21d87554b094480f872987a6d6d4d8ad36301308
gloria779 <glorianajjinda1@gmail.com>
2025-06-19 14:41:15 +0300

    roles.js

---
+++ middleware/roles.js
@@ -0,0 +1,9 @@
+function authorizeRoles(...roles) {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.sendStatus(403);
+    }
+    next();
+  };
+}
+module.exports = authorizeRoles;
